867e90adb0ebe573a6aacddc127dddf6a9b7ba5c
unknown <mcvilleanrapsody@gmail.com>
2022-02-04 20:24:17 +0800

    set up Empty Values Validation and Unique Field Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,10 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong, try again later",
+    msg: err.message || "Something went wrong, try again later",
   };
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
@@ -13,7 +13,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .map((item) => item.message)
       .join(",");
   }
-  // res.status(defaultError.statusCode).json({ msg: err })
+
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+
+  // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
