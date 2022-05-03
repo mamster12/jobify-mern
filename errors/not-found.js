fcdd568860dacb16735ce71c1a2883f6dc88663d
unknown <mcvilleanrapsody@gmail.com>
2022-05-03 15:52:31 +0800

    not-found.js modification

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import CustomAPIError from "./custom-api.js";
 
-class NotFoundError extends Error {
+class NotFoundError extends CustomAPIError {
   constructor(message) {
     super(message);
     this.statusCode = StatusCodes.NOT_FOUND;
